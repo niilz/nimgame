beb4af573746f3c5594b988ea2c7073a78cbf4a0
nilSinger <niilz@outlook.de>
2022-06-11 14:24:00 +0200

    Add GameService and Test

--- nimgame-backend/src/main/java/de/holisticon/vorsprechen/niilz/nimgame/service/GameService.java
+++ nimgame-backend/src/main/java/de/holisticon/vorsprechen/niilz/nimgame/service/GameService.java
@@ -1,2 +1,24 @@
-package de.holisticon.vorsprechen.niilz.nimgame.service;public class GameService {
+package de.holisticon.vorsprechen.niilz.nimgame.service;
+
+import de.holisticon.vorsprechen.niilz.nimgame.model.GameState;
+import org.springframework.stereotype.Service;
+
+/**
+ * Main Game functionality
+ */
+@Service
+public class GameService {
+
+    private final GameState gameState;
+
+    GameService() {
+        this.gameState = new GameState();
+    }
+
+    public boolean isGameStarted() {
+        return this.gameState.isStarted();
+    }
+    public void startgame() {
+        this.gameState.setStarted(true);
+    }
 }
--- nimgame-backend/src/test/java/de/holisticon/vorsprechen/niilz/nimgame/service/GameServiceTest.java
+++ nimgame-backend/src/test/java/de/holisticon/vorsprechen/niilz/nimgame/service/GameServiceTest.java
@@ -1,4 +1,25 @@
+package de.holisticon.vorsprechen.niilz.nimgame.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = GameService.class)
 class GameServiceTest {
-  
+
+    private final GameService gameService;
+
+    @Autowired
+    public GameServiceTest(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @Test
+    void gameIsStartedAfterStartingTheGame() {
+        assertFalse(gameService.isGameStarted());
+        gameService.startgame();
+        assertTrue(gameService.isGameStarted());
+    }
 }
