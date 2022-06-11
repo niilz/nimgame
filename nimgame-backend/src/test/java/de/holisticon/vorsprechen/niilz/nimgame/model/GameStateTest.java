0da9697583102c85a812b60ac649eb96250d8a94
nilSinger <niilz@outlook.de>
2022-06-11 15:17:51 +0200

    Add GameStateTest for inital state-conditions

---
+++ nimgame-backend/src/test/java/de/holisticon/vorsprechen/niilz/nimgame/model/GameStateTest.java
@@ -0,0 +1,18 @@
+package de.holisticon.vorsprechen.niilz.nimgame.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GameStateTest {
+
+    @Test
+    void initialGameStateIsCorrect() {
+        var initalGameState = new GameState();
+        var expectedGameState = GameState.State.STOPPED;
+        assertEquals(expectedGameState, initalGameState.getState());
+        var expectedMatchCount = GameState.INITIAL_MATCH_COUNT;
+        assertEquals(expectedMatchCount, initalGameState.getMatches());
+    }
+
+}
