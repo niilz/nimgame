3e4196487356d30382ed27afdb8d9114a34d1759
nilSinger <niilz@outlook.de>
2022-06-11 13:46:10 +0200

    Add project-layout (setup) for nimgame-backend

---
+++ nimgame-backend/src/main/java/de/holisticon/vorsprechen/niilz/nimgame/NimgameApplication.java
@@ -0,0 +1,13 @@
+package de.holisticon.vorsprechen.niilz.nimgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimgameApplication.class, args);
+	}
+
+}
---
+++ nimgame-backend/src/test/java/de/holisticon/vorsprechen/niilz/nimgame/NimgameApplicationTests.java
@@ -0,0 +1,13 @@
+package de.holisticon.vorsprechen.niilz.nimgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
