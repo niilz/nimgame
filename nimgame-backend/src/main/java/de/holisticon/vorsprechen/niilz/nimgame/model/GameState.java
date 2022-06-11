54220959d61b36a3856a803fbf6b3f24b4e41719
nilSinger <niilz@outlook.de>
2022-06-11 15:09:43 +0200

    Initalize Game-State with matches

--- nimgame-backend/src/main/java/de/holisticon/vorsprechen/niilz/nimgame/model/GameState.java
+++ nimgame-backend/src/main/java/de/holisticon/vorsprechen/niilz/nimgame/model/GameState.java
@@ -3,7 +3,17 @@ package de.holisticon.vorsprechen.niilz.nimgame.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class GameState {
+
+    private final int INITIAL_MATCH_COUNT = 13;
+    private Integer matches;
+
+    GameState() {
+        this.matches = INITIAL_MATCH_COUNT;
+    }
 
     public enum State {
         RUNNING, STOPPED, WON,
