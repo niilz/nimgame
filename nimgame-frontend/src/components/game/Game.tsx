4b901c1f7ac28dc5ef8a3af4d863920ae199470b
nilSinger <niilz@outlook.de>
2022-06-14 17:47:24 +0200

    Add Match-Picker

--- nimgame-frontend/src/components/game/Game.tsx
+++ nimgame-frontend/src/components/game/Game.tsx
@@ -1,8 +1,9 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { GameState } from "../../model/GameState";
 import { GameStateMessage } from "../../model/GameStateMessage";
 import { Matches } from "../matches/Matches";
 import { Player } from "../player/Player";
-import styles from "./components/Game.module.css";
+import styles from "./Game.module.css";
 
 type GameProps = {
   state: GameStateMessage;
@@ -11,10 +12,21 @@ type GameProps = {
 };
 
 export function Game(props: GameProps) {
+  const [matches, setMatches] = useState("1");
   return (
-    <div>
+    <div className={styles.Game}>
       <Matches remainingMatches={props.state.currentMatchCount} />
       <Player player={props.state.player} playerType={props.state.type} />
+      <label className={styles.Label}>{matches}</label>
+      <input
+        type="range"
+        min="1"
+        max="3"
+        value={matches}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setMatches(e.target.value)
+        }
+      />
       {props.state.gameState == GameState.RUNNING ? (
         <button onClick={props.onRestart}>restart</button>
       ) : (
