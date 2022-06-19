961406df92e6a39d39ccc9068310a28a8625e2bf
nilSinger <niilz@outlook.de>
2022-06-19 17:15:49 +0200

    Add obvious winning choices

--- smart-computer/src/main.rs
+++ smart-computer/src/main.rs
@@ -38,28 +38,55 @@ impl MoveCollector {
     fn collect_all_games(&mut self, moves: &[Condition], remaining_matches: u8) {
         let current_player = self.player;
         let next_player = swap_player(current_player);
-        if remaining_matches == 0 {
-            // No more matches: last player looses (last player drew the last match)
-            // So th player tha would play now (current_player) wins
-            let finished_game = Game {
-                winner: current_player,
-                moves: moves.to_vec(),
-            };
-            self.games.push(finished_game);
-            return;
-        }
-        // Draw one 1, 2 and 3 matches at this point in the game
-        for drawn in 1..=3 {
-            self.player = next_player;
-            if drawn <= remaining_matches {
-                self.counter += 1;
-                let mut this_moves = moves.to_vec();
-                this_moves.push(Condition {
+        match remaining_matches {
+            // This Player should draw all but one and will win,
+            // because next player has to draw 1
+            2..=4 => {
+                let second_to_last_move = Condition {
                     player: current_player,
-                    drawn,
+                    drawn: remaining_matches - 1,
                     remaining: remaining_matches,
-                });
-                self.collect_all_games(&this_moves, remaining_matches - drawn);
+                };
+                let mut finished_game_moves = moves.to_vec();
+                finished_game_moves.push(second_to_last_move);
+                let last_move = Condition {
+                    player: next_player,
+                    drawn: 1,
+                    remaining: 1,
+                };
+                finished_game_moves.push(last_move);
+                let finished_game = Game {
+                    winner: current_player,
+                    moves: finished_game_moves,
+                };
+                self.games.push(finished_game);
+                return;
+            }
+            _ => {
+                if remaining_matches == 0 {
+                    // No more matches: last player looses (last player drew the last match)
+                    // So th player tha would play now (current_player) wins
+                    let finished_game = Game {
+                        winner: current_player,
+                        moves: moves.to_vec(),
+                    };
+                    self.games.push(finished_game);
+                    return;
+                }
+                // Draw one 1, 2 and 3 matches at this point in the game
+                for drawn in 1..=3 {
+                    self.player = next_player;
+                    if drawn <= remaining_matches {
+                        self.counter += 1;
+                        let mut this_moves = moves.to_vec();
+                        this_moves.push(Condition {
+                            player: current_player,
+                            drawn,
+                            remaining: remaining_matches,
+                        });
+                        self.collect_all_games(&this_moves, remaining_matches - drawn);
+                    }
+                }
             }
         }
     }
@@ -91,7 +118,6 @@ fn main() {
     // Determine which positions have the highest chance ONE
     let mut weights_one = HashMap::new();
     for game in games_won_by_one {
-        println!("{game:#?}");
         for condition in game.moves.iter().step_by(2) {
             *weights_one.entry(condition).or_insert(0) += 1;
         }
@@ -99,7 +125,6 @@ fn main() {
     // Determine which positions have the highest chance for player TWO
     let mut weights_two = HashMap::new();
     for game in games_won_by_two {
-        println!("{game:#?}");
         for condition in game.moves[1..].iter().step_by(2) {
             *weights_two.entry(condition).or_insert(0) += 1;
         }
@@ -114,10 +139,6 @@ fn main() {
             .iter()
             .filter(|(condition, _weight)| condition.remaining == remaining_matches)
             .map(|(condition, weight)| ((condition.drawn, weight)))
-            .map(|(drawn, weight)| {
-                println!("remaining: {remaining_matches}, drawn: {drawn}, weight: {weight}");
-                (drawn, weight)
-            })
             .max_by(|(_draw_a, weight_a), (_draw_b, weight_b)| weight_a.cmp(weight_b))
         {
             best_conditions_one.insert(remaining_matches, best_draw);
